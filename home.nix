e1538bff90d130212cc2e502862988ec0c96fb46
Youwen Wu <youwenw@gmail.com>
2024-07-11 12:01:02 -0700

    feat: basic waybar rice

--- home.nix
+++ home.nix
@@ -250,8 +250,6 @@
       "blur,swaync-control-center"
       "ignorezero,swaync-control-center"
       "blur,logout_dialog"
-
-      "blur,waybar"
     ];
     monitor = [
       "DP-1,2560x1440@165,1920x0,auto"
@@ -338,17 +336,95 @@
       mainBar = {
         layer = "top";
         position = "top";
-        height = 30;
-        output = [
-          "DP-1"
-          "HDMI-A-1"
-        ];
-        modules-left = [ "hyprland/workspaces" "sway/submap" "wlr/taskbar" ];
-        modules-right = [ "mpd" "temperature" ];
-
+        height = 24;
+        width = 1600;
+        reload-style-on-change = true;
+        margin = "10px 0px 0px 0px";
+        modules-left = [ "hyprland/window" "hyprland/workspaces" ];
+        modules-right = [ "backlight" "group/adjustable" "custom/weather" ];
+        modules-center = [ "network" "group/hardware" "clock" ];
         "hyprland/workspaces" = {
-          disable-scroll = true;
-          all-outputs = true;
+          active-only = false;
+          all-outputs = false;
+          format = "{icon}";
+          format-icons = {
+            default = "";
+            active = "";
+          };
+          persistent-workspaces = {
+            eDP-1 = [ 1 ];
+            DP-1 = [ 2 3 4 5 ];
+          };
+        };
+        "group/hardware" = {
+          orientation = "inherit";
+          modules = [ "cpu" "memory" "battery" ];
+        };
+        "group/adjustable" = {
+          orientation = "inherit";
+          drawer = {
+            transition-duration = 500;
+            transition-left-to-right = true;
+          };
+          modules = [ "pulseaudio" "mpris" ];
+        };
+        "custom/weather" = {
+          orientation = "horizontal";
+          exec = "curl wttr.in/?format=\"%l:%20%t\"";
+          interval = 10;
+        };
+        cpu = {
+          interval = 10;
+          format = "{usage}%  ";
+        };
+        memory = {
+          interval = 10;
+          format = "{percentage}%  ";
+        };
+        # mpris = {
+        #   format-playing = "    {title}  ";
+        #   format-paused = " 󰏤 {title}  ";
+        #   format-stopped = "Nothing Playing";
+        # };
+        tray = {
+          spacing = 10;
+        };
+        clock = {
+          format = "{:%a %b %d, %I:%M %p} ";
+        };
+        backlight = {
+          device = "intel_backlight";
+          format = "{percent}% {icon}";
+          format-icons = [ "󰃞" "󰃠" ];
+        };
+        battery = {
+          states = {
+            good = 95;
+            warning = 20;
+            critical = 10;
+          };
+          format = "{capacity}% {icon}";
+          format-icons = [ "󰁻" "󰁽" "󰁿" "󰂀" "󰁹" ];
+        };
+        "hyprland/window" = {
+          format = "{class}";
+        };
+        network = {
+          format-wifi = "{essid} ";
+          format-linked = "{ifname} (No IP) ";
+          format-disconnected = "Disconnected ⚠";
+          format-alt = "{ifname}: {ipaddr}/{cidr}";
+        };
+        pulseaudio = {
+          format = "{volume}% {icon}  {format_source}";
+          format-bluetooth = "{volume}% {icon}  {format_source}";
+          format-bluetooth-muted = " {icon} {format_source}";
+          format-muted = " {format_source}";
+          format-source = "{volume}% ";
+          format-source-muted = "";
+          format-icons = {
+            default = [ "" "" "" ];
+          };
         };
       };
     };
