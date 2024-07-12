88c89e9b6c4dd6b059f0370276d06d69f7d0712e
Youwen Wu <youwenw@gmail.com>
2024-07-11 20:14:52 -0700

    feat: add window rule to vesktop

--- home.nix
+++ home.nix
@@ -220,7 +220,7 @@
       "opacity 0.80 0.80,class:^(com.obsproject.Studio)$ # Obs-Qt"
       "opacity 0.80 0.80,class:^(gnome-boxes)$ # Boxes-Gtk"
       "opacity 0.80 0.80,class:^(discord)$ # Discord-Electron"
-      "opacity 0.80 0.80,class:^(WebCord)$ # WebCord-Electron"
+      "opacity 0.80 0.80,class:^(vesktop)$ # Vesktop-Electron"
       "opacity 0.80 0.80,class:^(ArmCord)$ # ArmCord-Electron"
       "opacity 0.80 0.80,class:^(app.drey.Warp)$ # Warp-Gtk"
       "opacity 0.80 0.80,class:^(net.davidotek.pupgui2)$ # ProtonUp-Qt"
