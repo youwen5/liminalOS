5fca8afe3e77f416a3e619a9b2ac56314a5fcf2a
Youwen Wu <youwenw@gmail.com>
2024-07-30 04:32:21 -0700

    feat: configure some fish plugins and utils

--- home.nix
+++ home.nix
@@ -40,7 +40,6 @@
     ripgrep # recursively searches directories for a regex pattern
     jq # A lightweight and flexible command-line JSON processor
     yq-go # yaml processor https://github.com/mikefarah/yq
-    eza # A modern replacement for ‘ls’
     fzf # A command-line fuzzy finder
     bat
     pavucontrol
@@ -79,6 +78,7 @@
     btop # replacement of htop/nmon
     iotop # io monitoring
     iftop # network monitoring
+    fd
 
     # system call monitoring
     strace # system call monitoring
@@ -551,6 +551,50 @@
       fish_vi_key_bindings
       set -g fish_greeting
     '';
+    plugins = [
+      {
+        name = "autopair";
+        src = pkgs.fetchFromGitHub {
+          owner = "jorgebucaran";
+          repo = "autopair.fish";
+          rev = "4d1752ff5b39819ab58d7337c69220342e9de0e2";
+          hash = "sha256-qt3t1iKRRNuiLWiVoiAYOu+9E7jsyECyIqZJ/oRIT1A=";
+        };
+      }
+      {
+        name = "fzf";
+        src = pkgs.fetchFromGitHub {
+          owner = "PatrickF1";
+          repo = "fzf.fish";
+          rev = "8920367cf85eee5218cc25a11e209d46e2591e7a";
+          hash = "sha256-T8KYLA/r/gOKvAivKRoeqIwE2pINlxFQtZJHpOy9GMM=";
+        };
+      }
+      {
+        name = "sponge";
+        src = pkgs.fetchFromGitHub {
+          owner = "meaningful-ooo";
+          repo = "sponge";
+          rev = "384299545104d5256648cee9d8b117aaa9a6d7be";
+          hash = "sha256-MdcZUDRtNJdiyo2l9o5ma7nAX84xEJbGFhAVhK+Zm1w=";
+        };
+      }
+      {
+        name = "done";
+        src = pkgs.fetchFromGitHub {
+          owner = "franciscolourenco";
+          repo = "done";
+          rev = "eb32ade85c0f2c68cbfcff3036756bbf27a4f366";
+          hash = "sha256-DMIRKRAVOn7YEnuAtz4hIxrU93ULxNoQhW6juxCoh4o=";
+        };
+      }
+    ];
+  };
+
+  programs.eza = {
+    enable = true;
+    enableFishIntegration = true;
+    enableBashIntegration = true;
   };
 
   programs.bash.enable = true;
