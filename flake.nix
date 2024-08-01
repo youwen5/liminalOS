5cac7d28fbb7dbb214f2ae9d10546821b17fd4cb
Youwen Wu <youwenw@gmail.com>
2024-08-01 01:50:48 -0700

    chore: run nix-fmt

--- flake.nix
+++ flake.nix
@@ -17,13 +17,14 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.90.0.tar.gz";
+      url =
+        "https://git.lix.systems/lix-project/nixos-module/archive/2.90.0.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, catppuccin, lanzaboote, stablepkgs, lix-module
-    , ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, catppuccin, lanzaboote, stablepkgs
+    , lix-module, ... }@inputs: {
       formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt;
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs; };
