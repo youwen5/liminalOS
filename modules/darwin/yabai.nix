e9badf4d3d7129c6cc096d3669491c051397b62c
Youwen Wu <youwenw@gmail.com>
2024-08-05 17:04:54 -0700

    feat: add more yabai configuration

--- modules/darwin/yabai.nix
+++ modules/darwin/yabai.nix
@@ -4,14 +4,21 @@
     enableScriptingAddition = true;
     config = {
       focus_follows_mouse = "autoraise";
-      mouse_follows_focus = "off";
+      mouse_follows_focus = "on";
       window_placement = "second_child";
       window_opacity = "off";
-      top_padding = 36;
-      bottom_padding = 10;
-      left_padding = 10;
-      right_padding = 10;
+      top_padding = 12;
+      bottom_padding = 12;
+      left_padding = 12;
+      right_padding = 12;
       window_gap = 10;
+      split_ratio = 0.50;
+      split_type = "auto";
+      layout = "bsp";
+      mouse_modifier = "fn";
+      mouse_action1 = "move";
+      mouse_action2 = "resize";
+      mouse_drop_action = "swap";
     };
   };
   services.skhd = { enable = true; };
