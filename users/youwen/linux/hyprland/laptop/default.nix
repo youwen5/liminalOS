4a840c8faef639d2c918722ef9a0432180ff1b41
Youwen Wu <youwenw@gmail.com>
2024-08-07 08:17:21 -0700

    feat: add some laptop configuration for hyprland

--- users/youwen/linux/hyprland/laptop/default.nix
+++ users/youwen/linux/hyprland/laptop/default.nix
@@ -1,1 +1,18 @@
-{ imports = [ ../common.nix ./laptop-only.nix ]; }
+{ lib, ... }:
+{ 
+  imports = [ ../common.nix ]; 
+  wayland.windowManager.hyprland.settings.input.touchpad = {
+    natural_scroll = true;
+    disable_while_typing = true;
+    clickfinger_behavior = true;
+    tap-to-click = false;
+    scroll_factor = 0.5;
+  };
+  wayland.windowManager.hyprland.settings.general.sensitivity = lib.mkForce 1.0;
+  wayland.windowManager.hyprland.settings.env = [
+    "HYPRCURSOR_THEME,Bibata-Modern-Ice"
+    "HYPRCURSOR_SIZE,24"
+    "XCURSOR_THEME,Bibata-Modern-Ice"
+    "XCURSOR_SIZE,24"
+  ];
+}
