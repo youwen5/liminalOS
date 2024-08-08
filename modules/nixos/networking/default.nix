e387b8a0acd484f45d4a8272a7a6fb028c0f6208
Youwen Wu <youwenw@gmail.com>
2024-08-07 19:26:09 -0700

    feat(nixos): use cloudflare 1.1.1.1 nameservers

--- modules/nixos/networking/default.nix
+++ modules/nixos/networking/default.nix
@@ -2,4 +2,5 @@
   services.openssh.enable = true;
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
+  networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
 }
