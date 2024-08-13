47a2603989810181448738abde6f1eae7704fc88
Youwen Wu <youwenw@gmail.com>
2024-08-12 20:13:56 -0700

    feat: add nvim + kitty integration

--- users/youwen/linux/programs/default.nix
+++ users/youwen/linux/programs/default.nix
@@ -10,6 +10,13 @@
       shell_integration = "enabled";
       enable_audio_bell = "no";
       background_opacity = "0.8";
+      allow_remote_control = "socket-only";
+      listen_on = "unix:/tmp/kitty";
+      action_alias = "kitty_scrollback_nvim kitten /home/youwen/.local/share/nvim/lazy/kitty-scrollback.nvim/python/kitty_scrollback_nvim.py";
+    };
+    keybindings = {
+        "kitty_mod+h" = "kitty_scrollback_nvim";
+        "kitty_mod+g" = "kitty_scrollback_nvim --config ksb_builtin_last_cmd_output";
     };
   };
 
