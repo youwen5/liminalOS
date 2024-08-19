b1c8959a85e3d534123d80576445c4f0fcf2b798
Youwen Wu <youwenw@gmail.com>
2024-08-19 00:21:53 -0700

    feat: add harpoon config for neovim

--- users/youwen/common/neovim/default.nix
+++ users/youwen/common/neovim/default.nix
@@ -487,6 +487,28 @@
           open_for_directories = true;
         };
       };
+      harpoon = {
+        enable = true;
+        keymaps = {
+          addFile = "<Leader>hh";
+          cmdToggleQuickMenu = "<Leader>hm";
+          gotoTerminal = {
+            "1" = "<Leader>ht";
+            "2" = "<Leader>2";
+            "3" = "<Leader>3";
+            "4" = "<Leader>4";
+          };
+          navFile = {
+            "1" = "<Leader>ha";
+            "2" = "<Leader>hs";
+            "3" = "<Leader>hd";
+            "4" = "<Leader>hf";
+          };
+          navNext = "<Leader>h]";
+          navPrev = "<Leader>h[";
+          toggleQuickMenu = "<Leader>hk";
+        };
+      };
 
       # Code formatting
       conform-nvim = {
