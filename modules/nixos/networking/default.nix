dc86e970e772855d3875e731b3624188259df1a1
Youwen Wu <youwenw@gmail.com>
2024-08-23 23:47:22 -0700

    networking: open firewall ports for grim dawn

--- modules/nixos/networking/default.nix
+++ modules/nixos/networking/default.nix
@@ -1,6 +1,19 @@
-{
+let
+  universalAllowedPorts = [27016 42805];
+  universalAllowedRanges = [
+    {
+      from = 42852;
+      to = 42872;
+    }
+  ];
+in {
   services.openssh.enable = true;
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
+  networking.firewall = {
+    allowedTCPPorts = universalAllowedPorts;
+    allowedUDPPorts = universalAllowedPorts;
+    allowedUDPPortRanges = universalAllowedRanges;
+    allowedTCPPortRanges = universalAllowedRanges;
+  };
+  networking.firewall.enable = true;
   networking.nameservers = ["1.1.1.1" "1.0.0.1"];
 }
