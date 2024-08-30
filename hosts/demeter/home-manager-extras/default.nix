4bf9eaadfb22923566ad0a45a33c8cb858ff2313
Youwen Wu <youwenw@gmail.com>
2024-08-29 21:01:45 -0700

    feat(demeter): disable explicit sync

--- hosts/demeter/home-manager-extras/default.nix
+++ hosts/demeter/home-manager-extras/default.nix
@@ -5,4 +5,5 @@
 }: {
   wayland.windowManager.hyprland.settings.monitor = ["DP-1,2560x1440@165,1920x0,auto" "HDMI-A-1,1920x1080@60,0x0,1"];
   # wayland.windowManager.hyprland.package = inputs.stablepkgs.legacyPackages.${pkgs.system}.hyprland;
+  wayland.windowManager.hyprland.settings.render.explicit_sync = 0;
 }
