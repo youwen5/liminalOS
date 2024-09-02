21f3af9aad90fa5a657ded189daed43ae7d6bea9
Youwen Wu <youwenw@gmail.com>
2024-09-02 12:58:04 -0700

    fix: remove redundant xdg-desktop-portal-hyprland package

--- modules/linux/desktop-portal/default.nix
+++ modules/linux/desktop-portal/default.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
   xdg.portal = {
     enable = true;
-    extraPortals = with pkgs; [xdg-desktop-portal-hyprland xdg-desktop-portal-gtk];
+    extraPortals = with pkgs; [xdg-desktop-portal-gtk];
   };
 }
