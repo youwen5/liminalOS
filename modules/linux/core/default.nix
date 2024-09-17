10e0484f162f067712e04b12836e934469a2ddc7
Youwen Wu <youwenw@gmail.com>
2024-09-17 02:15:08 -0700

    feat: add nix store optimization and gc options

--- modules/linux/core/default.nix
+++ modules/linux/core/default.nix
@@ -28,5 +28,16 @@
       "nix-command"
       "flakes"
     ];
+    optimise.automatic = true;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 14d";
+    };
+    # Free up to 1GiB when there is less than 100MiB left
+    extraOptions = ''
+      min-free = ${toString (100 * 1024 * 1024)}
+      max-free = ${toString (1024 * 1024 * 1024)}
+    '';
   };
 }
