851091abe2eb2b88cbd4f0a4c8294289395ea73c
Youwen Wu <youwenw@gmail.com>
2024-09-22 11:37:38 -0700

    feat(adrastea): re-enable prime

--- hosts/adrastea/configuration.nix
+++ hosts/adrastea/configuration.nix
@@ -107,15 +107,15 @@
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
-    # prime = {
-    #   amdgpuBusId = "PCI:4:0:0";
-    #   nvidiaBusId = "PCI:1:0:0";
-    #   # offload = {
-    #   #   enable = true;
-    #   #   enableOffloadCmd = true;
-    #   # };
-    #   sync.enable = true;
-    # };
+    prime = {
+      amdgpuBusId = "PCI:4:0:0";
+      nvidiaBusId = "PCI:1:0:0";
+      # offload = {
+      #   enable = true;
+      #   enableOffloadCmd = true;
+      # };
+      sync.enable = true;
+    };
   };
 
   hardware.graphics.enable = true;
