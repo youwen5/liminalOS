5a56185f220e2a71500de3aa11bc38a10d0bee8f
Youwen Wu <youwenw@gmail.com>
2024-09-21 20:36:06 -0700

    feat(adrastea)!: configure new monitor setup

--- hosts/adrastea/home-manager-extras/default.nix
+++ hosts/adrastea/home-manager-extras/default.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }:
 {
   wayland.windowManager.hyprland.settings.monitor = pkgs.lib.mkForce [
-    "eDP-1,2560x1440@165,0x0,1.6"
+    # "eDP-1,2560x1440@165,0x0,1.6"
+    "eDP-1, disable"
+    "DP-3,2560x1440@165,0x0,1.0"
   ];
 }
