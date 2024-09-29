dabb36ee5fc47e048d0a49babcac88c947c7dab7
Youwen Wu <youwenw@gmail.com>
2024-09-29 01:51:24 -0700

    feat: add printing to core

--- hosts/demeter/configuration.nix
+++ hosts/demeter/configuration.nix
@@ -114,9 +114,6 @@
     xkb.variant = "";
   };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
