b9b29fb478ae275fd423a352d31fe93befac0410
Youwen Wu <youwenw@gmail.com>
2024-09-29 17:31:35 -0700

    feat(adrastea): use new hdmi display

--- hosts/adrastea/home-manager-extras/default.nix
+++ hosts/adrastea/home-manager-extras/default.nix
@@ -3,6 +3,6 @@
   wayland.windowManager.hyprland.settings.monitor = pkgs.lib.mkForce [
     # "eDP-1,2560x1440@165,0x0,1.6"
     "eDP-1, disable"
-    "DP-3,2560x1440@165,0x0,1.0"
+    "HDMI-A-1,2560x1440@144,0x0,1.0"
   ];
 }
