06e032f5de5a587369e384c12fc78923053f3b37
Youwen Wu <youwenw@gmail.com>
2024-10-10 23:20:44 -0700

    feat(adrastea): enable flatpaks

--- hosts/adrastea/configuration.nix
+++ hosts/adrastea/configuration.nix
@@ -49,6 +49,8 @@
   #   };
   # };
 
+  services.flatpak.enable = true;
+
   boot.initrd.luks.devices."luks-52d1be6d-b32f-41e0-a6d7-2ff52599fe7c".device = "/dev/disk/by-uuid/52d1be6d-b32f-41e0-a6d7-2ff52599fe7c";
 
   services.tlp.enable = true;
