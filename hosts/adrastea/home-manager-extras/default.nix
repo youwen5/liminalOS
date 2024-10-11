bf23ffa30c03300fdf6b700c535983f05b45922d
Youwen Wu <youwenw@gmail.com>
2024-10-10 21:30:23 -0700

    fix: disable hardware cursors on adrastea to stop choppiness

--- hosts/adrastea/home-manager-extras/default.nix
+++ hosts/adrastea/home-manager-extras/default.nix
@@ -5,6 +5,7 @@
     "eDP-1, disable"
     "HDMI-A-1,2560x1440@144,0x0,1.0"
   ];
+  wayland.windowManager.hyprland.settings.cursor.no_hardware_cursors = true;
 
   # since we are using this as a "desktop" of sorts, we have no need to save
   # power by using optimus. poor performance on external display, so we add
