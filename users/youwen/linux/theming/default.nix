03482ac8eec8d06945adc82d24f77d1022797927
Youwen Wu <youwenw@gmail.com>
2024-10-29 13:56:10 -0700

    feat: update gtk and qt ricing

--- users/youwen/linux/theming/default.nix
+++ users/youwen/linux/theming/default.nix
@@ -4,7 +4,6 @@
 
   gtk = {
     enable = true;
-    catppuccin.enable = true;
     cursorTheme = {
       name = "Bibata-Modern-Ice";
       size = 26;
@@ -12,20 +11,24 @@
     iconTheme = {
       name = "Papirus-Dark";
     };
+    theme = {
+      name = "Tokyonight-Dark";
+      package = pkgs.tokyonight-gtk-theme;
+    };
   };
 
   qt = {
     enable = true;
-    platformTheme.name = "qtct";
-    style.name = "kvantum";
+    platformTheme.name = "gtk";
+    style.name = "gtk2";
   };
 
-  xdg.configFile = {
-    "Kvantum/kvantum.kvconfig".text = ''
-      [General]
-      theme=GraphiteNordDark
-    '';
-
-    "Kvantum/GraphiteNord".source = "${pkgs.graphite-kde-theme}/share/Kvantum/GraphiteNord";
-  };
+  # xdg.configFile = {
+  #   "Kvantum/kvantum.kvconfig".text = ''
+  #     [General]
+  #     theme=GraphiteNordDark
+  #   '';
+  #
+  #   "Kvantum/GraphiteNord".source = "${pkgs.graphite-kde-theme}/share/Kvantum/GraphiteNord";
+  # };
 }
