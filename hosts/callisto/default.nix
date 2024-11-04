1c11d431c9deeb6996e566671298a9a07e68b87a
Youwen Wu <youwenw@gmail.com>
2024-11-04 00:38:55 -0800

    fix: import correct modules for callisto

--- hosts/callisto/default.nix
+++ hosts/callisto/default.nix
@@ -4,37 +4,45 @@
   ...
 }:
 {
-  imports = with inputs; [
-    ./configuration.nix
-    ../../modules/linux/audio
-    ../../modules/linux/networking
-    ../../modules/linux/fonts
-    ../../modules/linux/greeter
-    ../../modules/linux/core
-    ../../modules/linux/desktop-portal
-    ../../modules/linux/stylix
-    ../../overlays
+  imports =
+    with inputs;
+    [
+      ./configuration.nix
+      ../../modules/linux/audio
+      ../../modules/linux/networking
+      ../../modules/linux/fonts
+      ../../modules/linux/greeter
+      ../../modules/linux/core
+      ../../modules/linux/desktop-portal
+      ../../modules/linux/stylix
+      ../../overlays
+      {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.backupFileExtension = "backup";
+        home-manager.extraSpecialArgs = {
+          inherit inputs;
+          inherit system;
+        };
+        home-manager.users.youwen = {
+          imports =
+            [
+              ../../users/youwen/common
+              ../../users/youwen/linux/laptop
+              ../../users/youwen/linux/packages/aarch-64
+              ../../users/youwen/common/fastfetch
+              ./home-manager-extras
+            ]
+            ++ (with inputs; [
+              nix-index-database.hmModules.nix-index
+            ]);
+        };
+      }
+    ]
+    ++ (with inputs; [
+      lix-module.nixosModules.default
+      home-manager.nixosModules.home-manager
+      apple-silicon.nixosModules.apple-silicon-support
 
-    apple-silicon.nixosModules.apple-silicon-support
-    lix-module.nixosModules.default
-    home-manager.nixosModules.home-manager
-    {
-      home-manager.useGlobalPkgs = true;
-      home-manager.useUserPackages = true;
-      home-manager.backupFileExtension = "backup";
-      home-manager.extraSpecialArgs = {
-        inherit inputs;
-        inherit system;
-      };
-      home-manager.users.youwen = {
-        imports = [
-          ../../users/youwen/common
-          ../../users/youwen/linux/laptop
-          ../../users/youwen/linux/packages/aarch-64
-          ../../users/youwen/common/fastfetch
-          ./home-manager-extras
-        ];
-      };
-    }
-  ];
+    ]);
 }
