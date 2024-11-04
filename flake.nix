46e9621dbb7f6f0623918ab3578e6fbe207e5853
Youwen Wu <youwenw@gmail.com>
2024-11-04 11:56:28 -0800

    chore: switch back to nixos-unstable

--- flake.nix
+++ flake.nix
@@ -2,8 +2,7 @@
   description = "System configuration flake.";
 
   inputs = {
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     # stablepkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     # bleedingpkgs.url = "github:nixos/nixpkgs/master";
     # nixpkgs-unstable-small.url = "github:nixos/nixpkgs/nixos-unstable-small";
