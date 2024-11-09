ae7223b2137a31ca152df32933ff8eb457bde317
Youwen Wu <youwenw@gmail.com>
2024-11-08 20:57:45 -0800

    refactor: move mime-apps for all architectures

--- users/youwen/linux/packages/x86_64/default.nix
+++ users/youwen/linux/packages/x86_64/default.nix
@@ -23,17 +23,6 @@ in
       ++ [ zen-browser ]
     );
 
-  xdg.mimeApps = {
-    enable = true;
-    defaultApplications = {
-      "text/html" = [ "zen.desktop" ];
-      "x-scheme-handler/http" = [ "zen.desktop" ];
-      "x-scheme-handler/https" = [ "zen.desktop" ];
-      "x-scheme-handler/about" = [ "zen.desktop" ];
-      "x-scheme-handler/unknown" = [ "zen.desktop" ];
-    };
-  };
-
   home.sessionVariables = {
     DEFAULT_BROWSER = "${zen-browser}/bin/zen";
   };
--- users/youwen/linux/programs/default.nix
+++ users/youwen/linux/programs/default.nix
@@ -33,6 +33,17 @@
     };
   };
 
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = [ "zen.desktop" ];
+      "x-scheme-handler/http" = [ "zen.desktop" ];
+      "x-scheme-handler/https" = [ "zen.desktop" ];
+      "x-scheme-handler/about" = [ "zen.desktop" ];
+      "x-scheme-handler/unknown" = [ "zen.desktop" ];
+    };
+  };
+
   # programs.firefox = {
   #   enable = true;
   #   # package = pkgs.librewolf;
