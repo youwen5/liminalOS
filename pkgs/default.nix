75370d69d3acc096a55b94a525eab7b574b467b1
Youwen Wu <youwenw@gmail.com>
2024-12-15 02:57:39 -0800

    fix: add extra wrapper vars for modrinth app to avoid segfault

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,6 +3,5 @@ let
   inherit (pkgs) callPackage;
 in
 final: prev: {
-  zen-browser-unwrapped = callPackage ./by-name/zen-browser-unwrapped/package.nix { };
-  zen-browser = callPackage ./by-name/zen-browser/package.nix { };
+  modrinth-app = callPackage ./by-name/modrinth-app { };
 }
