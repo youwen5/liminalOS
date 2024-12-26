4f975307456a6adf0715770153e47cb0a6862d59
Youwen Wu <youwenw@gmail.com>
2024-12-26 00:54:19 -0800

    fix: re-enable gtk theming

--- hm/modules/linux/theming/stylix.nix
+++ hm/modules/linux/theming/stylix.nix
@@ -1,13 +1,22 @@
 { osConfig, lib, ... }:
 {
-  config.stylix.targets = lib.mkIf osConfig.liminalOS.theming.enable {
-    waybar.enable = false;
-    kitty.variant256Colors = true;
-    neovim.enable = false;
-    kde.enable = true;
-    gnome.enable = true;
-    swaync.enable = false;
-    hyprlock.enable = false;
-    hyprland.enable = false;
+  config = lib.mkIf osConfig.liminalOS.theming.enable {
+    stylix.targets = {
+      waybar.enable = false;
+      kitty.variant256Colors = true;
+      neovim.enable = false;
+      kde.enable = true;
+      gnome.enable = true;
+      swaync.enable = false;
+      hyprlock.enable = false;
+      hyprland.enable = false;
+    };
+
+    gtk = {
+      enable = true;
+      iconTheme = {
+        name = "Papirus-Dark";
+      };
+    };
   };
 }
