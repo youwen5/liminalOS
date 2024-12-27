5777eb41c948eca3b8438216ec98f1c509837847
Youwen Wu <youwenw@gmail.com>
2024-12-26 22:30:56 -0800

    Revert 1 commits
    5733da3 'feat: use nix-flatpak fork that allows local path for flatpakref'

--- modules/linux/gaming/default.nix
+++ modules/linux/gaming/default.nix
@@ -50,8 +50,12 @@ in
 
       services.flatpak.packages = lib.mkIf cfg.roblox.enable [
         {
-          flatpakref = ./sober.flatpakref;
+          flatpakref = "./sober.flatpakref";
+          sha256 = "sha256:1pj8y1xhiwgbnhrr3yr3ybpfis9slrl73i0b1lc9q89vhip6ym2l";
+        }
+        {
           appId = "org.vinegarhq.Sober";
+          origin = "sober";
         }
       ];
 
