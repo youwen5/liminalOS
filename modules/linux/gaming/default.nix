f5985b0c04ae3ee6ebeb032756eb197910ff198e
Youwen Wu <youwenw@gmail.com>
2024-12-26 21:52:35 -0800

    chore: vendor the sober flatpakref to prevent github actions 403

--- modules/linux/gaming/default.nix
+++ modules/linux/gaming/default.nix
@@ -50,7 +50,7 @@ in
 
       services.flatpak.packages = lib.mkIf cfg.roblox.enable [
         {
-          flatpakref = "https://sober.vinegarhq.org/sober.flatpakref";
+          flatpakref = "./sober.flatpakref";
           sha256 = "sha256:1pj8y1xhiwgbnhrr3yr3ybpfis9slrl73i0b1lc9q89vhip6ym2l";
         }
         {
