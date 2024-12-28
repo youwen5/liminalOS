7bcbbe7ecb43f10892d28623848a3ee1b08faa08
Youwen Wu <youwenw@gmail.com>
2024-12-27 21:14:03 -0800

    chore: rekey agenix for callisto

--- reference/secrets/authorized_keys.nix
+++ reference/secrets/authorized_keys.nix
@@ -1,12 +1,16 @@
 rec {
   users = builtins.attrValues {
-    youwen = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIwqDFdb/cs5K9gsgP0ogyuq5pv9hSxsyPnDcWc5wRKs";
+    "youwen@demeter" =
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIwqDFdb/cs5K9gsgP0ogyuq5pv9hSxsyPnDcWc5wRKs";
+    "youwen@callisto" =
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA+0w7l4jWN+b3Cqs9pjzjxUt2tRXk8HPIB3sqfUQMdx";
     runner = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEEBFBqlbHn3gMuV0i8U48xctZUWXkmHsCK1O6LRpXpj";
   };
 
   systems = builtins.attrValues {
     demeter = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP4BRdoxPnmlhMD1kI7qXwVE//6h1XWUnkwpzDuJaAyC";
     gallium = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJzDKscmZIz7GF0nfKpnKHq63/fwzx2PXir0mUtRDOgu";
+    callisto = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM7CqpsBe6+pAQa5CC8m2quJdDg6hRCJlTbVEcHN2xxV";
   };
 
   all = users ++ systems;
