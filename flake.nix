12ff7a820dffe683787a4fb07eea5665ab2ccf7c
Youwen Wu <youwenw@gmail.com>
2025-01-10 13:57:27 -0800

    fix: use nixos unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "System configuration flake.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     # stablepkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     # bleedingpkgs.url = "github:nixos/nixpkgs/master";
     # nixpkgs-unstable-small.url = "github:nixos/nixpkgs/nixos-unstable-small";
