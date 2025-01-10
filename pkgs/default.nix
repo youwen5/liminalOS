fae30c2feb1a46755898b78a52229edfc7b47680
Youwen Wu <youwenw@gmail.com>
2025-01-10 14:39:29 -0800

    chore: reintegrate with main

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,4 +6,5 @@ final: prev: {
   modrinth-app = callPackage ./by-name/modrinth-app { };
   rdrview = callPackage ./by-name/rdrview { };
   wine-discord-ipc-bridge = callPackage ./by-name/wine-discord-ipc-bridge { };
+  hyprland-qtutils = callPackage ./by-name/hyprland-qtutils { };
 }
