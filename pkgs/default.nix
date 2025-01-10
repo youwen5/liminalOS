0ca4e5d1ffe97c6102061d941d4b42330492875a
Youwen Wu <youwenw@gmail.com>
2025-01-10 14:09:07 -0800

    chore: remove unnecessary hyprland-qtutils

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,7 +4,6 @@ let
 in
 final: prev: {
   modrinth-app = callPackage ./by-name/modrinth-app { };
-  # hyprland-qtutils = callPackage ./by-name/hyprland-qtutils { };
   rdrview = callPackage ./by-name/rdrview { };
   wine-discord-ipc-bridge = callPackage ./by-name/wine-discord-ipc-bridge { };
 }
