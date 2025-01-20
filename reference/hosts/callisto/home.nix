4d941451875d95259b93cc210699754a64419bab
Youwen Wu <youwenw@gmail.com>
2025-01-20 13:41:40 -0800

    feat(callisto): disable explicit sync in asahi for honeykrisp

--- reference/hosts/callisto/home.nix
+++ reference/hosts/callisto/home.nix
@@ -7,4 +7,7 @@
   home.stateVersion = "24.05";
 
   liminalOS.desktop.hyprland.screenlocker.monitor = "eDP-1";
+
+  # explicit sync must be disabled for honeykrisp to load
+  wayland.windowManager.hyprland.settings.render.explicit_sync = 0;
 }
