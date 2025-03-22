6cb988a735e206007dad1330ad8aad20bb80073c
Youwen Wu <youwenw@gmail.com>
2025-03-22 12:35:38 -0700

    feat: upgrade lzbt

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
     };
 
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.4.1";
+      url = "github:nix-community/lanzaboote/v0.4.2";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-parts.follows = "flake-parts";
     };
