0e3e8df85695c53f2d1c8a8dbea59f0de12ce07d
Youwen Wu <youwenw@gmail.com>
2025-03-22 12:35:47 -0700

    chore: update monitor configuration for demeter

--- reference/hosts/demeter/home.nix
+++ reference/hosts/demeter/home.nix
@@ -7,8 +7,7 @@
   home.stateVersion = "24.05";
 
   wayland.windowManager.hyprland.settings.monitor = [
-    "DP-1,2560x1440@144,1920x0,auto"
-    "HDMI-A-1,1920x1080@60,0x0,1"
+    "DP-1,2560x1440@144,0x0,auto"
   ];
 
   liminalOS.desktop.hyprland.screenlocker.monitor = "DP-1";
