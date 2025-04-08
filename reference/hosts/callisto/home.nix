9f58a2560d5d300ca31bda59a9c6acf8c3c85876
Youwen Wu <youwenw@gmail.com>
2025-04-08 13:08:49 -0700

    fix(callisto): use screenlocker crash fix

--- reference/hosts/callisto/home.nix
+++ reference/hosts/callisto/home.nix
@@ -7,6 +7,7 @@
   home.stateVersion = "24.05";
 
   liminalOS.desktop.hyprland.screenlocker.monitor = "eDP-1";
+  liminalOS.desktop.hyprland.screenlocker.useCrashFix = true;
 
   # explicit sync must be disabled for honeykrisp to load
   wayland.windowManager.hyprland.settings.render.explicit_sync = 0;
