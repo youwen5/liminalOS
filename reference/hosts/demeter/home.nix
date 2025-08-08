d0bf3e067c87eb1bf4227dad968663ed959149f3
Youwen Wu <youwenw@gmail.com>
2025-08-08 13:55:05 -0700

    feat: enable hdr on demeter

--- reference/hosts/demeter/home.nix
+++ reference/hosts/demeter/home.nix
@@ -7,8 +7,9 @@
   home.stateVersion = "24.05";
 
   wayland.windowManager.hyprland.settings.monitor = [
-    "DP-1,2560x1440@165,0x0,auto,bitdepth,10,cm,hdr"
+    "DP-1,2560x1440@165,0x0,auto,bitdepth,10,cm,hdr,sdrbrightness,1.35,sdrsaturation,1.325"
   ];
+  wayland.windowManager.hyprland.settings.misc.vrr = 3;
 
   liminalOS.desktop.hyprland.screenlocker.monitor = "DP-1";
 }
