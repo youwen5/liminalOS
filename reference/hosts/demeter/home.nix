2840d05bcf6068e2901cd482fe923010136635e6
Youwen Wu <youwenw@gmail.com>
2025-08-23 02:09:41 -0700

    fix(demeter): reference correct monitor

--- reference/hosts/demeter/home.nix
+++ reference/hosts/demeter/home.nix
@@ -7,9 +7,9 @@
   home.stateVersion = "24.05";
 
   wayland.windowManager.hyprland.settings.monitor = [
-    "DP-1,2560x1440@165,0x0,auto,bitdepth,10"
+    "DP-3,2560x1440@165,0x0,auto,bitdepth,10"
   ];
   wayland.windowManager.hyprland.settings.misc.vrr = 3;
 
-  liminalOS.desktop.hyprland.screenlocker.monitor = "DP-1";
+  liminalOS.desktop.hyprland.screenlocker.monitor = "DP-3";
 }
